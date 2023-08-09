ad0773d9732adf25f5ec6b2e494383901ac3bdff
yeyeJared <gjared388@gmail.com>
2023-08-09 16:26:18 -0600

    [ADD] model user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,38 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDb } from "@/utils/database";
+import { User } from "@/models/user";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDb();
-      return true
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      //if not create a user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace("", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
     } catch (err) {
       console.error(err);
       return false;
